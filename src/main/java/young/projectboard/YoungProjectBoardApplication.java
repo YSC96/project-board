9ee5e8f94f590cb6d98f8d64f1b5e43e3329400f
ysccc <ewa111@naver.com>
2022-09-03 17:17:01 +0900

    #1 - 스프링 부트 설정

---
+++ src/main/java/young/projectboard/YoungProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package young.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungProjectBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/young/projectboard/YoungProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package young.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
