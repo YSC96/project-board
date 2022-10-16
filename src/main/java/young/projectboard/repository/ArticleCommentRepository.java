e5d3f3a361f0de112f7cbe480d8c6128400429d9
ysccc <ewa111@naver.com>
2022-10-16 14:49:29 +0900

    #3 - Spring Data REST + HAL Explorer 디펜던시 추가 api 를 간편하게 만들기 위해 스프링 부트의 기능을 추가

--- src/main/java/young/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/young/projectboard/repository/ArticleCommentRepository.java
@@ -1,7 +1,10 @@
 package young.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import young.projectboard.domain.ArticleComment;
 
+@RepositoryRestResource // spring data rest 사용하기 위한 annotation
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
 }
--- src/main/java/young/projectboard/repository/ArticleRepository.java
+++ src/main/java/young/projectboard/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package young.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import young.projectboard.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
